a274ba3b2891a534ec8833dd7449446f62195001
Bozhkata <138572444+BozhkataS@users.noreply.github.com>
2024-11-25 11:50:37 +0200

    CreateGameScreen

--- app/(main)/(tabs)/play/CreateGameScreen.tsx
+++ app/(main)/(tabs)/play/CreateGameScreen.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, TextInput, FlatList, ImageBackground, ScaledSize, TouchableOpacity, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, ScaledSize, TouchableOpacity, Dimensions, Modal, TextInput, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { AntDesign, FontAwesome6 } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 import { Picker } from '@react-native-picker/picker';
-import { BackgroundLayer, StyleSource } from '@maplibre/maplibre-react-native';
 
 export default function CreateGameScreen() {
   const navigation = useNavigation();
   const [screenWidth, setScreenWidth] = useState(Dimensions.get('window').width);
   const [screenHeight, setScreenHeight] = useState(Dimensions.get('window').height);
+  const [showSettings, setShowSettings] = useState(false);
   const [selectedOption, setSelectedOption] = useState('random');
-  const [minutes, setMinutes] = useState('1');
+  const [minutes, setMinutes] = useState('');
   const [seconds, setSeconds] = useState('');
 
   useEffect(() => {
@@ -24,20 +24,26 @@ export default function CreateGameScreen() {
     };
   }, []);
 
+  const handleCloseSettings = () => {
+    setShowSettings(false);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.backgroundContainer}>
-        { 
         <ImageBackground
-          source={require('../../../../assets/images/forest3.png')}
+          source={require('../../../../assets/images/cyclists.png')}
           style={{
             width: screenWidth,
             height: screenHeight,
             flex: 1,
           }}
           resizeMode="cover"
-        /> }
+        />
+      </View>
+
+      <View style={styles.headingContainer}>
+        <Text style={styles.headingText}>Riders' Run</Text>
       </View>
 
       <View style={styles.backButtonContainer}>
@@ -46,83 +52,96 @@ export default function CreateGameScreen() {
         </TouchableOpacity>
       </View>
 
-      <View style={styles.threeContainer}>
-        <View style={styles.playersContainer}>
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Players</Text>
-        </View>
-        <FlatList
-          style={styles.namesContainer}
-          data={[
-           {name:'Oliver'},
-           {name:'Jack'},
-           {name:'Daniel'},
-           {name:'Alex'},
-                    ]}
-          renderItem={({item: {name}}) => (
-          <View style={styles.row} key={name}>
-            <FontAwesome6 name="user-circle" size={20} color="black"/>
-            <Text style={styles.name}>   {name}</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.startGameButton}>
+          <Text style={styles.startGameText}>Start Game</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.settingsButton} onPress={() => setShowSettings(true)}>
+          <Text style={styles.settingsText}>Settings</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.playerSection}>
+        <ScrollView style={styles.playerScrollContainer} contentContainerStyle={styles.playerScrollContent}>
+          <View style={styles.player}>
+            <AntDesign name="user" size={20} color="white" style={styles.profileIcon} />
+            <Text style={styles.playerText}>Player 1</Text>
           </View>
-          )}
-          >
-          </FlatList>
-        </View>
-      
-        <View style={styles.settingsContainer}>
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Settings</Text>
-        </View>
-
-        <View style={styles.mainSettingsPart}>
-        
-          <Text style={styles.contentText}>Seeker selection:</Text>
-          
-          <Picker
-            selectedValue={selectedOption}
-            onValueChange={(itemValue) => setSelectedOption(itemValue)}
-            style={styles.picker}
-          >
-            <Picker.Item label="Random" value="random" />
-            <Picker.Item label="Custom" value="custom" />
-          </Picker>
-
-          <Text style={styles.contentText}>Waiting period:</Text>
-
-          <View style={styles.timeInputContainer}>
-            <TextInput
-              style={styles.textBoxTime}
-              keyboardType="numeric"
-              value={minutes}
-              onChangeText={setMinutes}
-              placeholder="0"
-            />
-            <Text style={styles.label}> m </Text>
-            <TextInput
-              style={styles.textBoxTime}
-              keyboardType="numeric"
-              value={seconds}
-              onChangeText={setSeconds}
-              placeholder="0"
-            />
-            <Text style={styles.label}> s </Text>
+          <View style={styles.player}>
+            <AntDesign name="user" size={20} color="white" style={styles.profileIcon} />
+            <Text style={styles.playerText}>Player 2</Text>
+          </View>
+          <View style={styles.player}>
+            <AntDesign name="user" size={20} color="white" style={styles.profileIcon} />
+            <Text style={styles.playerText}>Player 3</Text>
+          </View>
+          <View style={styles.player}>
+            <AntDesign name="user" size={20} color="white" style={styles.profileIcon} />
+            <Text style={styles.playerText}>Player 4</Text>
+          </View>
+          <View style={styles.player}>
+            <AntDesign name="user" size={20} color="white" style={styles.profileIcon} />
+            <Text style={styles.playerText}>Player 5</Text>
+          </View>
+          <View style={styles.player}>
+            <AntDesign name="user" size={20} color="white" style={styles.profileIcon} />
+            <Text style={styles.playerText}>Player 6</Text>
           </View>
 
-        </View>
-
-        </View>
-
-        <View style={styles.joinCodeContainer}>
-        <Text style={styles.textBox}>Code: {"\n"}DKASMD</Text>
+        </ScrollView>
+        <View style={styles.bottomIcons}>
+          <TouchableOpacity style={styles.iconButton}>
+            <AntDesign name="qrcode" size={20} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconButton}>
+            <AntDesign name="link" size={20} color="white" />
+          </TouchableOpacity>
         </View>
       </View>
 
-        <View style={styles.startButtonContainer}>
-        <TouchableOpacity style={styles.startButton} onPress={() => navigation.goBack()}>
-          <Text style={styles.buttonText}>Start Game</Text>
-        </TouchableOpacity>
+      <Modal transparent={true} visible={showSettings} onRequestClose={handleCloseSettings}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalHeaderText}>Settings</Text>
+              <TouchableOpacity onPress={handleCloseSettings}>
+                <AntDesign name="close" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.modalBody}>
+              <Text style={styles.label}>Select Option</Text>
+              <View style={styles.pickerContainer}>
+                <Picker selectedValue={selectedOption} style={styles.picker} itemStyle={styles.pickerItem} onValueChange={(itemValue) => setSelectedOption(itemValue)}>
+                  <Picker.Item label="Random" value="random" />
+                  <Picker.Item label="Custom" value="custom" />
+                </Picker>
+              </View>
+
+              <Text style={styles.label}>Set Timer</Text>
+              <View style={styles.timerInputContainer}>
+                <TextInput
+                  style={styles.timerInput}
+                  placeholder="Minutes"
+                  placeholderTextColor="rgba(255, 255, 255, 0.5)"
+                  keyboardType="numeric"
+                  value={minutes}
+                  onChangeText={setMinutes}
+                />
+                <Text style={styles.colon}>:</Text>
+                <TextInput
+                  style={styles.timerInput}
+                  placeholder="Seconds"
+                  placeholderTextColor="rgba(255, 255, 255, 0.5)"
+                  keyboardType="numeric"
+                  value={seconds}
+                  onChangeText={setSeconds}
+                />
+              </View>
+            </View>
+          </View>
         </View>
-      
+      </Modal>
     </View>
   );
 }
@@ -134,146 +153,207 @@ const styles = StyleSheet.create({
   },
   backgroundContainer: {
     position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-  threeContainer: {
-    position: 'relative',
-    top: 0,
-    marginTop: 150,
-    height: '50%',
-    width: '84%',
-    //backgroundColor: '#999999',
-  },
-  playersContainer: {
-    position: 'absolute',
-    left: 0,
-    backgroundColor: '#999999',
-    height: '75%',
-    width: '47%',
-    borderWidth: 2,
-    borderRadius: 10,
-    borderColor: '#fff',
-  },
-  row: {
-    marginLeft: '5%',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: "center",
-    marginVertical: 3,
-  },
-  name: {
-    fontSize: 17,
-  },
-  header: {
-    height: '10%',
-    width: '100%',
-    borderBottomWidth: 1.5,
-    borderColor: "#000",
-    textAlign: 'center',
-    justifyContent: 'center',
-  },
-  headerText: {
-    fontSize: 20,
-    textAlign: 'center',
-  },
-  namesContainer: {
-    flex: 1,
-    marginTop: '10%',
-  },
-  settingsContainer: {
-    position: 'absolute',
-    right: 0,
-    backgroundColor: '#999999',
-    height: '100%',
-    width: '45%',
-    borderWidth: 2,
-    borderColor: '#fff',
-    borderRadius: 10,
-  },
-  mainSettingsPart: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  contentText: {
-    marginTop: 20,
-    marginBottom: '10%',
-    fontSize: 17,
-  },
-  picker: {
-    width: '70%',
-    height: 50,
-    textAlign: 'center',
-  },
-  timeInputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 10,
-  },
-  label: {
-    fontSize: 17,
-  },
-  textBoxTime: {
-    padding: 10,
-    borderWidth: 1,
-    width: 30,
-    borderRadius: 5,
-    borderColor: "#000",
-    backgroundColor: '#fff',
-    textAlign: "center",
-  },  
-  joinCodeContainer: {
-    position: 'absolute',
-    bottom: 0,
-    marginBottom: 20,
-    height: '14%',
-    width: '47%',
-  },
-  textBox: {
-    height: '100%',
-    width: '100%',
-    borderWidth: 1.5,
-    borderColor: "#000",
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    textAlign: "center",
-    fontSize: 17,
-  },
-  startButtonContainer: {
-    //position: 'absolute',
-    marginTop: 100,
-    //bottom: 0,
-    //marginBottom: '5%',
-    height: '8.5%',
-    width: '50%',
-    justifyContent: 'center',
-    
-  },
-  startButton: {
-    flex: 1,
-    height: '100%',
-    width: '100%',
-    backgroundColor: '#20c967',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    borderColor: '#18994e',
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderTopWidth: 1,
-    borderBottomWidth: 10,
-  },
-  buttonText: {
-    fontSize: 17,
-    color: '#ffffff',
   },
   backButtonContainer: {
     position: 'absolute',
     top: 15,
     left: 15,
   },
-  backButton: {
+  backButton: {},
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 50,
+    right: 30,
+    alignItems: 'center',
+  },
+  headingContainer: {
+    marginTop: 130,
+  },
+  headingText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'rgb(230, 230, 0)',
+    textShadowColor: '#000',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 3,
+  },
+  settingsButton: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderWidth: 3,
+    borderColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: 'rgb(51, 173, 255)',
+    shadowRadius: 20,
+    marginTop: 20,
+    backgroundColor: 'rgb(51, 173, 255)',
+  },
+  startGameButton: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderWidth: 3,
+    borderColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: 'rgb(255, 51, 51)',
+    shadowRadius: 20,
+    backgroundColor: 'rgb(255, 51, 51)',
+  },
+  settingsText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontFamily: 'Apple Chancery, cursive',
+  },
+  startGameText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontFamily: 'Apple Chancery, cursive',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    width: '80%',
+    backgroundColor: '#101723',
+    borderRadius: 10,
+    padding: 20,
+    borderWidth: 3,
+    borderColor: 'white',
+  },
+  modalHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 2,
+    borderBottomColor: '#ccc',
+    paddingBottom: 10,
+  },
+  modalHeaderText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#f5f0f0',
+    fontFamily: 'Apple Chancery, cursive',
+  },
+  modalBody: {
+    marginTop: 20,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 10,
+    color: '#f5f0f0',
+    fontFamily: 'Apple Chancery, cursive',
+  },
+  pickerContainer: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    overflow: 'hidden',
+    marginBottom: 20,
+    width: '100%',
+  },
+  picker: {
+    height: 50,
+    width: '100%',
+    color: '#f5f0f0',
+    backgroundColor: '#101723',
+  },
+  pickerItem: {
+    height: 50,
+    width: '100%',
+    color: '#f5f0f0',
+  },
+  timerInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  timerInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    margin: 5,
+    textAlign: 'right',
+    flex: 1,
+    width: 70,
+    color: '#f5f0f0',
+    backgroundColor: '#101723',
+  },
+  colon: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginHorizontal: 5,
+    color: '#f5f0f0',
+  },
+  playerSection: {
+    position: 'absolute',
+    bottom: 50,
+    left: 30,
+    width: 220,
+    height: 220,
+    borderWidth: 2,
+    borderColor: 'white',
+    borderRadius: 20,
+    backgroundColor: 'transparent',
+    overflow: 'hidden',
+    padding: 5,
+  },
+  playerScrollContainer: {
+    flex: 1,
+  },
+  playerScrollContent: {
+    paddingBottom: 40,
+  },
+  player: {
+    width: '100%',
+    height: 35,
+    borderRadius: 20,
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    borderBottomColor: 'white',
+    borderTopColor: 'white',
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    marginBottom: 2,
+    flexDirection: 'row',
+  },
+  profileIcon: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  playerText: {
+    fontSize: 14,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  bottomIcons: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    height: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: 220,
+    paddingHorizontal: 10,
+    backgroundColor: '#101723',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  iconButton: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderWidth: 2,
+    borderColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
   },
 });
