f24969bb06e3834738d93268b0d0e5a3d1ba22d5
Yasen88hd <yasen88hd@gmail.com>
2024-11-26 17:28:10 +0200

    updated play page

--- app/(main)/(tabs)/play/index.tsx
+++ app/(main)/(tabs)/play/index.tsx
@@ -1,55 +1,102 @@
-import React, { useState, useEffect } from 'react';
-import { ImageBackground, StyleSheet, View, Text, Dimensions, ScaledSize, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native'; 
-import { useRouter } from 'expo-router';
+import React, { useState, useEffect } from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  ScaledSize,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
+import { router } from "expo-router";
 
 export default function HomeScreen() {
-  const [screenWidth, setScreenWidth] = useState(Dimensions.get('window').width);
-  const [screenHeight, setScreenHeight] = useState(Dimensions.get('window').height);
+  const [code, setCode] = useState("");
+
+  const [screenWidth, setScreenWidth] = useState(
+    Dimensions.get("window").width
+  );
+  const [screenHeight, setScreenHeight] = useState(
+    Dimensions.get("window").height
+  );
 
   useEffect(() => {
     const handleResize = (dims: { window: ScaledSize; screen: ScaledSize }) => {
       setScreenWidth(dims.window.width);
       setScreenHeight(dims.window.height);
     };
-    const subscription = Dimensions.addEventListener('change', handleResize);
+    const subscription = Dimensions.addEventListener("change", handleResize);
     return () => {
       subscription?.remove();
     };
   }, []);
 
-  const navigation = useRouter();
-
   const handleCreateGame = () => {
-    navigation.navigate('/play/CreateGameScreen');
+    fetch("http://localhost:5000/api/create-game", {
+      method: "POST",
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.text();
+        }
+      })
+      .then((data) => {
+        //get game code
+        console.log(data);
+        let code: string = data as string;
+
+        //store code somewhere
+
+        router.push("/play/CreateGameScreen");
+      });
   };
 
   const handleJoinGame = () => {
-    navigation.navigate('/play/JoinGameScreen');
+    router.navigate("/play/JoinGameScreen");
   };
+
+  function handleCodeChanged(text: string) {
+    setCode(text.toUpperCase());
+  }
 
   return (
     <View style={styles.container}>
-      <View style={styles.backgroundContainer}>
-        { 
-        <ImageBackground
-          source={require('../../../../assets/images/proba.png')}
-          style={{
-            width: screenWidth,
-            height: screenHeight,
-            flex: 1,
-          }}
-          resizeMode="cover"
-        /> }
+      {/* Background */}
+      <View style={styles.background}>
+        <Image
+          source={require("../../../../assets/images/bikers.png")}
+          style={styles.backgroundImage}
+        />
       </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.buttonCreate} onPress={handleCreateGame}>
-          <Text style={styles.buttonText}>Create Game</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity style={styles.buttonJoin} onPress={handleJoinGame}>
-          <Text style={styles.buttonText}>Join Game</Text>
-        </TouchableOpacity>
+
+      {/* Main Menu */}
+      <View style={styles.mainMenu}>
+        <View style={styles.text}>
+          <Text style={styles.title}>Project Tag</Text>
+        </View>
+        <View style={styles.buttons}>
+          <TouchableOpacity style={styles.button} onPress={handleCreateGame}>
+            <Text style={styles.buttonText}>Create Game</Text>
+          </TouchableOpacity>
+          <View style={styles.joinContainer}>
+            <TextInput
+              style={styles.textBox}
+              value={code}
+              onChangeText={handleCodeChanged}
+              placeholder={"Enter code"}
+              maxLength={4}
+            />
+
+            <TouchableOpacity
+              style={[styles.button, styles.joinButton]}
+              onPress={handleJoinGame}
+            >
+              <Text style={styles.buttonText}>Join</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -59,51 +106,75 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  backgroundContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-  buttonContainer: {
+  background: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    position: "absolute",
+    width: "100%",
+    height: "100%",
   },
-  buttonCreate: {
-    marginTop: 80,
-    height: 100,
-    width: 250,
-    transform: 'rotateZ(-25deg)',
-    borderRadius: 10,
-    borderRightWidth: 1,
-    borderLeftWidth: 1,
-    borderBottomWidth: 14,
-    borderColor: '#2859de',
-    elevation: 30,
-    backgroundColor: '#1E90FF',
-    justifyContent: 'center',
-    alignItems: 'center',
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
   },
-  buttonJoin: {
-    marginTop: 120,
-    height: 100,
-    width: 250,
-    backgroundColor: '#20c967',
-    transform: 'rotateZ(25deg)',
-    borderRadius: 10,
-    borderRightWidth: 1,
-    borderLeftWidth: 1,
-    borderBottomWidth: 14,
-    borderColor: '#18994e',
-    elevation: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
+  mainMenu: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    marginBottom: 150,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+    fontFamily: "cursive",
+  },
+  buttons: {
+    flexDirection: "column",
+    alignItems: "center",
+    gap: 10,
+  },
+  button: {
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "rgb(21, 66, 117)",
+    borderRadius: 5,
+    width: 200,
+    height: 70,
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
+    fontSize: 21,
+    textAlign: "center",
+    color: "rgb(21, 66, 117)",
+    fontFamily: "cursive",
+    fontWeight: "bold",
+  },
+  joinContainer: {
+    flexDirection: "row",
+    borderRadius: 5,
+    width: 200,
+  },
+  textBox: {
+    borderWidth: 1,
+    borderColor: "rgb(21, 66, 117)",
+    borderRadius: 5,
+
+    padding: 12,
+    textAlign: "center",
+    width: 100,
+    height: 70,
+    backgroundColor: "#fff",
+    flex: 6,
+
+    fontWeight: 700,
+  },
+  joinButton: {
+    marginLeft: 10,
+    flex: 4,
   },
 });
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -6,8 +6,8 @@ import {
   TouchableOpacity,
   StyleSheet,
   Alert,
-} from 'react-native';
-import {useRouter} from "expo-router";
+} from "react-native";
+import { useRouter } from "expo-router";
 import jsSHA from "jssha";
 
 // Type for form data
@@ -22,9 +22,9 @@ export default function LoginScreen() {
   const router = useRouter();
   const [isSignUp, setIsSignUp] = useState<boolean>(false);
   const [formData, setFormData] = useState<FormData>({
-    username: '',
-    password: '',
-    confirmPassword: '',
+    username: "",
+    password: "",
+    confirmPassword: "",
   });
 
   // Handle input change
@@ -36,11 +36,11 @@ export default function LoginScreen() {
   const validateForm = () => {
     const { username, password, confirmPassword } = formData;
     if (!username || !password) {
-      Alert.alert('Error', 'Please fill in all required fields.');
+      Alert.alert("Error", "Please fill in all required fields.");
       return false;
     }
     if (isSignUp && password !== confirmPassword) {
-      Alert.alert('Error', 'Passwords do not match.');
+      Alert.alert("Error", "Passwords do not match.");
       return false;
     }
     return true;
@@ -48,64 +48,69 @@ export default function LoginScreen() {
 
   // Handle form submission
   const handleSubmit = async () => {
-
     var data = new FormData();
-    data.append('username', formData.username);
+    data.append("username", formData.username);
 
     const hash = new jsSHA("SHA-384", "TEXT");
     hash.update(formData.password);
-    var hashedPass = hash.getHash('HEX');
-    data.append('password', hashedPass);
+    var hashedPass = hash.getHash("HEX");
+    data.append("password", hashedPass);
 
-    if (validateForm()) {
-      var resp = await fetch('http://localhost:5000/api/login', {
-        method: 'POST',
+    if (!validateForm()) return;
+
+    if (isSignUp) {
+      fetch("http://localhost:5000/api/register", {
+        method: "POST",
         body: data,
+      }).then((resp) => {
+        if (resp.ok) {
+          router.replace("/(main)/");
+        }
       });
-
-      // Reset form after submission
-      setFormData({ username: '', password: '', confirmPassword: '' });
-
-      if (resp.ok) {
-        const action = isSignUp ? 'Sign Up' : 'Sign In';
-        Alert.alert(`${action} Successful`, `Welcome, ${formData.username}!`);
-        router.replace('/(main)/');
-      }
+    } else {
+      fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        body: data,
+      }).then((resp) => {
+        if (resp.ok) {
+          router.replace("/(main)/");
+        }
+      });
     }
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{isSignUp ? 'Sign Up' : 'Sign In'}</Text>
-      
+      <Text style={styles.title}>{isSignUp ? "Sign Up" : "Sign In"}</Text>
+
       <TextInput
         style={styles.input}
         placeholder="Username"
         value={formData.username}
-        onChangeText={(text) => handleInputChange('username', text)}
+        onChangeText={(text) => handleInputChange("username", text)}
       />
-      
+
       <TextInput
         style={styles.input}
         placeholder="Password"
         secureTextEntry
         value={formData.password}
-        onChangeText={(text) => handleInputChange('password', text)}
+        onChangeText={(text) => handleInputChange("password", text)}
       />
-      
+
       {isSignUp && (
         <TextInput
           style={styles.input}
           placeholder="Confirm Password"
           secureTextEntry
           value={formData.confirmPassword}
-          onChangeText={(text) => handleInputChange('confirmPassword', text)}
+          onChangeText={(text) => handleInputChange("confirmPassword", text)}
         />
       )}
 
       <TouchableOpacity style={styles.button} onPress={handleSubmit}>
         <Text style={styles.buttonText}>
-          {isSignUp ? 'Sign Up' : 'Sign In'}
+          {isSignUp ? "Sign Up" : "Sign In"}
         </Text>
       </TouchableOpacity>
 
@@ -114,55 +119,57 @@ export default function LoginScreen() {
         onPress={() => setIsSignUp(!isSignUp)}
       >
         <Text style={styles.switchModeText}>
-          {isSignUp ? 'Already have an account? Sign In' : 'Don’t have an account? Sign Up'}
+          {isSignUp
+            ? "Already have an account? Sign In"
+            : "Don’t have an account? Sign Up"}
         </Text>
       </TouchableOpacity>
     </View>
   );
-};
+}
 
 // Styles for the component
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
-    backgroundColor: '#f8f9fa',
+    backgroundColor: "#f8f9fa",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    color: '#333',
+    color: "#333",
   },
   input: {
-    width: '100%',
+    width: "100%",
     padding: 15,
     marginVertical: 10,
     borderRadius: 8,
-    borderColor: '#ddd',
+    borderColor: "#ddd",
     borderWidth: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   button: {
-    width: '100%',
+    width: "100%",
     padding: 15,
-    backgroundColor: '#007bff',
+    backgroundColor: "#007bff",
     borderRadius: 8,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 20,
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   switchModeButton: {
     marginTop: 20,
   },
   switchModeText: {
-    color: '#007bff',
+    color: "#007bff",
     fontSize: 14,
   },
 });
